dd2bb9b2af156ef51a0056f7b18d3f54d491bee3
Laiba25K-3025 <K253025@nu.edu.pk>
2025-10-24 09:36:23 +0500

    Add files via upload

---
+++ task 1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main (){
+	
+	int i,j,num,result;
+	printf("Enter number");
+	scanf("%d",&num);
+	
+	for(i = 1;i<=num;i++){
+		printf("number %d:\t",i);
+		for(j=1;j<=10;j++){
+			result= i*j;
+			printf("%4d",result);
+		}
+		printf("\n");
+		
+		
+	}
+	return 0;
+}
