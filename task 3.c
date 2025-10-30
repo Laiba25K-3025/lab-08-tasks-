5e83119817f8f3485ad06eabbc9eb03e3c389c60
Laiba25K-3025 <K253025@nu.edu.pk>
2025-10-30 20:27:33 +0500

    Add files via upload

---
+++ task 3.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int grid[4][4] = {
+        {12, 15, 10, 9},
+        {11,  8, 12, 13},
+        {14, 13,  9, 7},
+        {16, 11, 10, 8}
+    };
+    int i,j;
+   
+    printf("Cold spots location:\n");
+   
+    for( i = 0; i < 4; i++) {
+        for( j = 0; j < 4; j++) {
+
+            int value = grid[i][j];
+            int isCold = 1; 
+
+            
+            if(i > 0 && grid[i-1][j] <= value)
+                isCold = 0;
+
+            
+            if(i < 3 && grid[i+1][j] <= value)
+                isCold = 0;
+
+        
+            if(j > 0 && grid[i][j-1] <= value)
+                isCold = 0;
+
+            
+            if(j < 3 && grid[i][j+1] <= value)
+                isCold = 0;
+
+            
+            if(isCold) {
+                printf("- At position (%d,%d) with temperature %dC\n", i+1, j+1, value);
+            }
+        }
+    }
+
+    return 0;
+}
